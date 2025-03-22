ca9ec55a2d8b1c2f8dc1a2ee187b2d6b648bd2d4
Liza Nikitina <liza@hml.cz>
2025-03-22 21:45:13 +0100

    init project with vite React Tailwindcss Boilerplate

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="relative overflow-hidden bg-white">
+      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Welcome!
+          </h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
