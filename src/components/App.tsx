735e5d600f8b74b028352fe4c5f7aebcfd734f5d
Liza Nikitina <liza@hml.cz>
2025-03-23 21:30:25 +0100

    feat: add basic Input and Button components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
+import { Input } from './Input'
+import { Button } from './Button'
+
 function App() {
   return (
     <div className="relative overflow-hidden bg-white">
-      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
+      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48">
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Welcome!
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-2xl mb-8">
+            Hi! Type a github username or part of it to display information about matching users. By clicking on the name display their repos.
           </h1>
+          <div className="flex items-center justify-center">
+          <Input/>
+          <Button/>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <button className="px-6 py-2 text-white bg-black/90 rounded-lg hover:bg-black/80 focus:outline-none transition">
+      Search
+    </button>
+
+  )
+}
