ab445d500feb344028daae37d350ac08c2da3ae4
Liza Nikitina <liza@hml.cz>
2025-03-29 20:24:17 +0100

    fix mobile styles

--- src/views/UserSearchPage/index.tsx
+++ src/views/UserSearchPage/index.tsx
@@ -6,7 +6,7 @@ import { GitHubUsersList } from '../../components/GitHubUsersList'
 export const UserSearchPage = () => {
   return (
     <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-      <h1 className="mb-8 text-4xl font-bold tracking-tight text-gray-900 sm:text-2xl">
+      <h1 className="mb-8 text-lg font-bold tracking-tight text-gray-900 sm:text-2xl lg:text-4xl">
         Hi! Type a github username or part of it to display information about
         matching users. By clicking on the name display their repos.
       </h1>
